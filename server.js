0609fe838f25bb0fc2d8a41f463f75e15b876303
branxz07 <pixel0202@hotmail.com>
2024-09-09 23:17:48 -0600

    Testing in AWS

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const express = require('express');
 // const app with anything it needs
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world con express'))
+app.get('/', (req, res) => res.send('<h1>Express con html Testing</h1>'))
 app.listen(8100, () => {
     console.log('Server is running on port 3000');
     });
